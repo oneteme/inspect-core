127d8fbfa63eaef0dd08ce551b3574d7414689e2
u$f <usf.alami@gmail.com>
2023-04-03 23:20:04 +0200

    edit

--- src/main/java/org/usf/traceapi/core/RemoteTraceSender.java
+++ src/main/java/org/usf/traceapi/core/RemoteTraceSender.java
@@ -38,7 +38,7 @@ public final class RemoteTraceSender implements TraceSender {
 			catch(Exception e) {
 				log.warn("error while tracing request : {}", mr.getUrl(), e);
 			}
-		}, delay, unit); //wait 5s after sending results)
+		}, delay, unit);
 	}
 	
 }
