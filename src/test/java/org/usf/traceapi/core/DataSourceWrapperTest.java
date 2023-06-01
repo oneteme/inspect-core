79ab23cbabcb3ea40f8b49ea4eeff273af9d2dad
u$f <usf.alami@gmail.com>
2023-06-01 09:13:40 +0200

    edit

--- src/test/java/org/usf/traceapi/core/DataSourceWrapperTest.java
+++ src/test/java/org/usf/traceapi/core/DataSourceWrapperTest.java
@@ -10,7 +10,7 @@ class DataSourceWrapperTest {
 
 	@CsvSource({
 		"jdbc:teradata://TDHOST/CHARSET=UTF8;DATABASE=my_data;SLOB_RECEIVE_THRESHOLD=15000,TDHOST/my_data",
-		"jdbc:teradata://TDHOST/CHARSET=UTF8;SLOB_RECEIVE_THRESHOLD=15000,TDHOST",
+		"jdbc:teradata://TDHOST/CHARSET=UTF8,TDHOST",
 		"jdbc:oracle:thin:@//myoracle.db.server:1521/my_servicename,myoracle.db.server:1521/my_servicename",
 		"jdbc:mysql://mysql.db.server:3306/my_database?useSSL=false&serverTimezone=UTC,mysql.db.server:3306/my_database",
 		"jdbc:postgresql//10.121.242.56:5432/ome,10.121.242.56:5432/ome"
