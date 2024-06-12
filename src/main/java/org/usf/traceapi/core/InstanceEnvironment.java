8553f443a3445443168c560029fe73811a715e86
u$f <usf.alami@gmail.com>
2024-06-12 17:06:26 +0200

    edit

--- src/main/java/org/usf/traceapi/core/InstanceEnvironment.java
+++ src/main/java/org/usf/traceapi/core/InstanceEnvironment.java
@@ -1,7 +1,18 @@
 package org.usf.traceapi.core;
 
+import static java.lang.String.join;
+import static java.lang.System.getProperty;
+import static java.net.InetAddress.getLocalHost;
+import static java.time.Instant.now;
+import static java.util.Objects.isNull;
+import static java.util.Optional.ofNullable;
+import static org.usf.traceapi.core.Helper.log;
+import static org.usf.traceapi.core.InstantType.SERVER;
+
+import java.net.UnknownHostException;
 import java.time.Instant;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
@@ -14,7 +25,7 @@ import lombok.With;
  */
 @Getter
 @ToString
-@RequiredArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class InstanceEnvironment {
 	
 	private final String name; //project name
@@ -30,4 +41,31 @@ public class InstanceEnvironment {
 	private final Instant instant; //startup time
 	private final String collector; //spring-collector-xx, ng-collector-xx,..
 	//commit, branch !?
+	
+    public static InstanceEnvironment localInstance(String name, String version, String... envs) {
+    	return new InstanceEnvironment(name, version,
+				hostAddress(),
+				isNull(envs) ? null : join(",", envs),
+				getProperty("os.name"), //version ? window 10 / Linux
+				"java " + getProperty("java.version"),
+				getProperty("user.name"),
+				SERVER,
+				now(),
+				collectorID());
+	}
+
+	private static String hostAddress() {
+		try {
+			return getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			log.warn("error while getting host address", e);
+			return null;
+		}
+	}
+	
+	private static String collectorID() {
+		return "spring-collector-v" //use getImplementationTitle
+				+ ofNullable(TraceConfiguration.class.getPackage().getImplementationVersion())
+				.orElse("?");
+	}
 }
--- src/main/java/org/usf/traceapi/core/TraceConfiguration.java
+++ src/main/java/org/usf/traceapi/core/TraceConfiguration.java
@@ -1,19 +1,12 @@
 package org.usf.traceapi.core;
 
-import static java.lang.String.join;
-import static java.lang.System.getProperty;
-import static java.net.InetAddress.getLocalHost;
-import static java.time.Instant.now;
 import static java.util.Objects.isNull;
-import static java.util.Optional.ofNullable;
 import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
 import static org.springframework.core.Ordered.LOWEST_PRECEDENCE;
 import static org.usf.traceapi.core.Helper.basePackage;
 import static org.usf.traceapi.core.Helper.log;
-import static org.usf.traceapi.core.InstantType.SERVER;
+import static org.usf.traceapi.core.InstanceEnvironment.localInstance;
 import static org.usf.traceapi.core.TraceMultiCaster.register;
-
-import java.net.UnknownHostException;
 
 import javax.sql.DataSource;
 
@@ -50,7 +43,10 @@ public class TraceConfiguration implements WebMvcConfigurer {
 	private RestSessionFilter sessionFilter;
 	
 	public TraceConfiguration(Environment env, TraceConfigurationProperties config, @Value("${api.tracing.base-package:}") String pkg) {
-		var inst = currentInstance(env);
+		var inst = localInstance(
+				env.getProperty("spring.application.name"),
+				env.getProperty("spring.application.version"),
+				env.getActiveProfiles());
 		basePackage = pkg;
 		register(config.getHost().isBlank() 
         		? res-> {} // cache traces !?
@@ -99,33 +95,4 @@ public class TraceConfiguration implements WebMvcConfigurer {
     		}
 		};
     }
-
-    private static InstanceEnvironment currentInstance(Environment env) {
-    	return new InstanceEnvironment(
-				env.getProperty("spring.application.name"),
-				env.getProperty("spring.application.version"),
-				hostAddress(),
-				join(",", env.getActiveProfiles()),
-				getProperty("os.name"), //version ? window 10 / Linux
-				"java " + getProperty("java.version"),
-				getProperty("user.name"),
-				SERVER,
-				now(),
-				collectorID());
-	}
-
-	private static String hostAddress() {
-		try {
-			return getLocalHost().getHostAddress();
-		} catch (UnknownHostException e) {
-			log.warn("error while getting host address", e);
-			return null;
-		}
-	}
-	
-	private static String collectorID() {
-		return "spring-collector-v" //use getImplementationTitle
-				+ ofNullable(TraceConfiguration.class.getPackage().getImplementationVersion())
-				.orElse("?");
-	}
 }
