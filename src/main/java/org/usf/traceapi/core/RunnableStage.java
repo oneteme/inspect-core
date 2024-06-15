503ba4e652faa5db8845af76a5fc9f67e43291b6
u$f <usf.alami@gmail.com>
2024-06-15 03:42:08 +0200

    edit

---
+++ src/main/java/org/usf/traceapi/core/RunnableStage.java
@@ -0,0 +1,29 @@
+package org.usf.traceapi.core;
+
+import static java.util.Objects.nonNull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 
+ * @author u$f
+ *
+ */
+@Getter
+@Setter
+public class RunnableStage extends SessionStage implements MutableStage {
+
+	private String name; //method, title
+	private String location; //class, URL
+	private ExceptionInfo exception; 
+	
+	@Override
+	String prettyFormat() {
+		var s = name + "(" + location + ")";
+		if(nonNull(exception)) {
+			s += exception;
+		}
+		return s;
+	}
+}
