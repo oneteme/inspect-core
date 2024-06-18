9927dff7f97d424dcfbde938878d85fb26b06806
u$f <usf.alami@gmail.com>
2024-06-19 00:06:30 +0200

    edit

--- src/main/java/org/usf/traceapi/core/Mail.java
+++ src/main/java/org/usf/traceapi/core/Mail.java
@@ -19,6 +19,6 @@ public final class Mail {
 	private String[] from;
  	private String[] recipients; 
 	private String[] replyTo;
-	private int size; //size in bytes, -1 otherwise
+	private int size; //in bytes, -1 otherwise
 	
 }
