ad0657c1a5a3edd67064585d54207d158b3a025e
u$f <usf.alami@gmail.com>
2024-06-19 00:40:59 +0200

    edit

--- src/main/java/org/usf/traceapi/core/SessionLogger.java
+++ src/main/java/org/usf/traceapi/core/SessionLogger.java
@@ -1,6 +1,5 @@
 package org.usf.traceapi.core;
 
-import static java.util.Objects.nonNull;
 import static org.usf.traceapi.core.Helper.log;
 
 import java.util.Collection;
@@ -16,35 +15,41 @@ import org.usf.traceapi.core.ScheduledDispatcher.Dispatcher;
 public final class SessionLogger implements Dispatcher<Session> {
 	
 	@Override
-    public boolean dispatch(int attemps, List<Session> sessions) {
-		try {
-			for(var s : sessions) {
-				log.debug("| {}", s);
-				for(var req : s.getRequests()) {
-					printSessionStage(req);
-				}
-				for(var req : s.getQueries()) {
-					printSessionStage(req);
-					printRequestStages(req.getActions());
-				}
-				for(var req : s.getFtpRequests()) {
-					printSessionStage(req);
-					printRequestStages(req.getActions());
-				}
-				for(var req : s.getMailRequests()) {
-					printSessionStage(req);
-					printRequestStages(req.getActions());
-				}
-				for(var req : s.getStages()) {
-					printSessionStage(req);
+    public boolean dispatch(int attempts, List<Session> sessions) {
+		if(attempts == 1) { //only first attempt
+			try {
+				for(var s : sessions) {
+					printSession(s);
 				}
 			}
-		}
-		catch (Exception e) {
-			log.warn("error while loggin sessions {}", e.getMessage());
-		}
+			catch (Exception e) {
+				log.warn("error while loggin sessions {}", e.getMessage());
+			}
+		} // else !log
 		return true; //important! always returns true
     }
+	
+	private static void printSession(Session s) {
+		log.debug("| {}", s);
+		for(var req : s.getRequests()) {
+			printSessionStage(req);
+		}
+		for(var req : s.getQueries()) {
+			printSessionStage(req);
+			printRequestStages(req.getActions());
+		}
+		for(var req : s.getFtpRequests()) {
+			printSessionStage(req);
+			printRequestStages(req.getActions());
+		}
+		for(var req : s.getMailRequests()) {
+			printSessionStage(req);
+			printRequestStages(req.getActions());
+		}
+		for(var req : s.getStages()) {
+			printSessionStage(req);
+		}
+	}
 
 	private static void printSessionStage(SessionStage stg) {
 		log.debug("\t- {}", stg);
