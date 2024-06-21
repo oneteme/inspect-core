dad2459d2e0366a12b0b93da5527c98210d46b4f
u$f <usf.alami@gmail.com>
2024-06-22 00:17:57 +0200

    edit

--- src/main/java/org/usf/traceapi/rest/WebFluxConfiguration.java
+++ src/main/java/org/usf/traceapi/rest/WebFluxConfiguration.java
@@ -1,6 +1,7 @@
 package org.usf.traceapi.rest;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +12,12 @@ import org.springframework.context.annotation.Configuration;
  *
  */
 @Configuration
-@ConditionalOnProperty(prefix = "api.tracing", name = "enabled", havingValue = "true")
 @ConditionalOnClass(name="org.springframework.web.reactive.function.client.ExchangeFilterFunction")
+@ConditionalOnProperty(prefix = "inspect", name = "enabled", havingValue = "true")
 public class WebFluxConfiguration {
 
     @Bean
+    @ConditionalOnExpression("${inspect.track.rest-request:true}!=false")
     public WebClientFilter webClientFilter() { 
         return new WebClientFilter();
     }
