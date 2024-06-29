e93df017223cecae8d389d2c64e80393fbffbf94
u$f <usf.alami@gmail.com>
2024-06-30 01:54:19 +0200

    edit

--- src/test/java/org/usf/traceapi/jdbc/DatabaseStageTrackerTest.java
+++ src/test/java/org/usf/traceapi/jdbc/DatabaseStageTrackerTest.java
@@ -1,7 +1,8 @@
 package org.usf.traceapi.jdbc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.usf.traceapi.jdbc.DatabaseStageTracker.decodeURL;
+import static org.usf.traceapi.jdbc.JdbcURLDecoder.decode;
+
+import java.util.Arrays;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -9,19 +10,87 @@ import org.junit.jupiter.params.provider.CsvSource;
 class DatabaseStageTrackerTest {
 	
 	@ParameterizedTest
-	@CsvSource(nullValues = {""}, value={
-			"jdbc:teradata://TDHOST/CHARSET=UTF8;DATABASE=my_data;SLOB_RECEIVE_THRESHOLD=15000,TDHOST,,my_data",
-			"jdbc:teradata://TDHOST:666/database=my_data;CHARSET=UTF8;SLOB_RECEIVE_THRESHOLD=15000,TDHOST,666,my_data",
-			"jdbc:teradata://TDHOST/CHARSET=UTF8,TDHOST,,",
-			"jdbc:oracle:thin:@//myoracle.db.server:1521/my_servicename,myoracle.db.server,1521,my_servicename",
-			"jdbc:mysql://mysql.db.server:3306/my_database?useSSL=false&serverTimezone=UTC,mysql.db.server,3306,my_database",
-			"jdbc:postgresql//10.123.321.44:4455/db,10.123.321.44,4455,db"})
-	void testShortURL(String origin, String host, String port, String schema) {
-		var arr = decodeURL(origin);
-		assertEquals(host, arr[0]);
-		assertEquals(port, arr[1]);
-		assertEquals(schema, arr[2]);
+	@CsvSource(nullValues="", value={
+		"jdbc:postgresql://localhost:5432/mydatabase,localhost,5432,mydatabase",
+		"jdbc:postgresql://192.168.1.100:5432/sampledb,192.168.1.100,5432,sampledb",
+		"jdbc:postgresql://db.example.com:5432/sampledb,db.example.com,5432,sampledb",
+		"jdbc:postgresql://localhost/mydatabase?user=testuser&password=testpass,localhost,,mydatabase",
+		"jdbc:postgresql://192.168.1.100/sampledb?ssl=true,192.168.1.100,,sampledb",
+		"jdbc:postgresql://db.example.com/sampledb?charSet=UTF8,db.example.com,,sampledb",
+		"jdbc:postgresql://localhost:5432/,localhost,5432,",
+		"jdbc:postgresql://192.168.1.100:5432,192.168.1.100,5432,",
+		"jdbc:postgresql://db.example.com:5432,db.example.com,5432,",
+	})
+	void testDecode_postgresql(String origin, String host, String port, String database) {
+		
+		var arr = decode(origin);
+		System.out.println(origin + "\t" + Arrays.toString(arr));
+		
+//		assertEquals("postgresql", arr[0]);
+//		assertEquals(host, arr[1]);
+//		assertEquals(port, arr[2]);
+//		assertEquals(database, arr[3]);
 	}
+	
+	@ParameterizedTest
+	@CsvSource(nullValues="", value={
+		"jdbc:teradata://hostname:1025/database=mydatabase",
+		"jdbc:teradata://192.168.1.100:5432/database=mydatabase",
+		"jdbc:teradata://db.example.com:8000/database=mydatabase",
+		"jdbc:teradata://hostname:1025/database=mydatabase,user=username,password=password",
+		"jdbc:teradata://192.168.1.100:5432/database=mydatabase,user=testuser,password=testpass",
+		"jdbc:teradata://db.example.com:8000/database=mydatabase,user=admin,password=secret",
+		"jdbc:teradata://hostname:1025/",
+		"jdbc:teradata://192.168.1.100:5432/",
+		"jdbc:teradata://db.example.com:8000/",
+	})
+	void testDecode_teradata(String origin) {
+
+		var arr = decode(origin);
+		System.out.println(origin + "\t" + Arrays.toString(arr));
+		
+//		var arr = decode(origin);
+//		assertEquals("postgresql", arr[0]);
+//		assertEquals(host, arr[1]);
+//		assertEquals(port, arr[2]);
+//		assertEquals(database, arr[3]);
+	}
+	
+	@ParameterizedTest
+	@CsvSource(nullValues="", value={
+		"jdbc:sqlserver://hostname:port;databaseName=mydatabase;encrypt=true;trustServerCertificate=true;",
+		"jdbc:sqlserver://hostname:port;encrypt=true;trustServerCertificate=true;databaseName=mydatabase;",
+		"jdbc:sqlserver://hostname:port;encrypt=true;databaseName=mydatabase;trustServerCertificate=true;",
+	})
+	void testDecode_sqlServer(String origin) {
+
+		var arr = decode(origin);
+		System.out.println(origin + "\t" + Arrays.toString(arr));
+		
+//		var arr = decode(origin);
+//		assertEquals("postgresql", arr[0]);
+//		assertEquals(host, arr[1]);
+//		assertEquals(port, arr[2]);
+//		assertEquals(database, arr[3]);
+	}
+	
+	@ParameterizedTest
+	@CsvSource(nullValues="", value={
+		"jdbc:h2:mem:testdb",
+		"jdbc:h2:file:/path/to/database",
+	})
+	void testDecode_h2(String origin) {
+
+		var arr = decode(origin);
+		System.out.println(origin + "\t" + Arrays.toString(arr));
+		
+//		var arr = decode(origin);
+//		assertEquals("postgresql", arr[0]);
+//		assertEquals(host, arr[1]);
+//		assertEquals(port, arr[2]);
+//		assertEquals(database, arr[3]);
+	}
+	
 	
 //	static final String query = "dummy query";
 //	static final Instant[] period = new Instant[2];
