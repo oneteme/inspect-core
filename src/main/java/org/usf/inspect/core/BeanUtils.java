dc21ae46dfa3a2d2ed9e347b9f7bca1a646d9c84
u$f <usf.alami@gmail.com>
2025-08-12 16:45:55 +0200

    edit

---
+++ src/main/java/org/usf/inspect/core/BeanUtils.java
@@ -0,0 +1,22 @@
+package org.usf.inspect.core;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public final class BeanUtils {
+	
+	public static void logLoadingBean(String name, Class<?> clazz) {
+    	log.info("[inspect-monitoring] loading {} (type: {})..", name, clazz);
+	}
+	
+	public static void logRegistringBean(String name, Class<?> clazz) {
+    	log.info("[inspect-monitoring] registering {} (type: {})..", name, clazz);
+	}
+
+	public static void logWrappingBean(String name, Class<?> clazz) {
+    	log.info("[inspect-monitoring] wrapping {} (type: {})..", name, clazz);
+	}
+}
