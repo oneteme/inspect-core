4273c35f40f6cd4b0186049bcf7b5e39f9bfb0d4
u$f <usf.alami@gmail.com>
2026-09-04 23:37:37 +0200

    edit

--- src/main/java/org/usf/inspect/core/AtomicExecutionListener.java
+++ src/main/java/org/usf/inspect/core/AtomicExecutionListener.java
@@ -1,85 +1,34 @@
 package org.usf.inspect.core;
 
-import static java.time.Clock.systemUTC;
-import static java.util.Objects.isNull;
-import static java.util.Objects.nonNull;
-import static org.usf.inspect.core.SessionContextManager.clearContext;
-import static org.usf.inspect.core.SessionContextManager.setActiveContext;
 import static org.usf.inspect.core.TraceDispatcherHub.hub;
 
 import java.time.Instant;
 
 import org.usf.inspect.core.InspectExecutor.ExecutionListener;
-import org.usf.inspect.core.SafeCallable.SafeConsumer;
+import org.usf.inspect.core.SafeCallable.SafeBiConsumer;
 
-import lombok.AccessLevel;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-/**
- * 
- * @author u$f
- *
- */
-@Getter(value = AccessLevel.PROTECTED)
-@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
-public abstract class AtomicExecutionListener implements Monitor2 {
+@RequiredArgsConstructor
+public class ModifiableExecutionListener<T> implements ExecutionListener<T> {
 	
-	public <R> ExecutionListener<R> executionListener(SafeConsumer<TraceSignal> cons) {
-		return executionListener(systemUTC().instant(), cons);
-	}
+	private final TraceUpdate update;
+	private final ExecutionListener<T> listener;
 	
-	public <R> ExecutionListener<R> executionListener(Instant start, SafeConsumer<TraceSignal> cons) {
-		return executionListener(createSignal(start, cons));
-	}
-	
-	public <R> ModifiableExecutionListener<R> modifiableExecutionListener(SafeConsumer<TraceSignal> cons) {
-		return modifiableExecutionListener(systemUTC().instant(), cons);
-	}
-	
-	public <R> ModifiableExecutionListener<R> modifiableExecutionListener(Instant start, SafeConsumer<TraceSignal> cons) {
-		var sgn = createSignal(start, cons);
-		hub().emitTrace(sgn);
-		var upd = update(sgn);
-		return new ModifiableExecutionListener<>(upd, executionListener(upd));
-	}
-	
-	protected TraceSignal createSignal(Instant start, SafeConsumer<TraceSignal> cons) {
-		var sgn = signal(start);
-		try {
-			cons.accept(sgn);
-		}
-		catch (Exception e) {
-			hub().reportError(true, this.getClass() + ".createSignal", e);
-		}
-		return sgn;
+	public ExecutionListener<T> map(SafeBiConsumer<TraceUpdate, T> cons) {
+		return (s,e,o,t)->{
+			try {
+				cons.accept(update, o); //execute before
+			}
+			catch (Exception ex) {
+				hub().reportError(true, "ModifiableExecutionListener.map", ex);
+			}
+			handle(s, e, o, t);
+		};
 	}
 
-	protected <R> ExecutionListener<R> executionListener(TraceSignal sgn) {
-		hub().emitTrace(sgn);
-		return executionListener(update(sgn));
-	}
-	
-	protected <R> ExecutionListener<R> executionListener(TraceUpdate upd) {
-		if(upd instanceof AbstractSessionUpdate ctx) {
-			setActiveContext(ctx);
-		}
-		return isNull(upd) ? Monitor2.noUpdateExecutionListenner() : (s,e,o,t)-> {
-			upd.setStart(s); //real method start
-			if(nonNull(t)) {
-				t = exception(t);
-				upd.setStatus(resolveStatus(t));
-				var exp = exceptionTrace(t, upd, e.toEpochMilli());
-				hub().emitTrace(exp);
-			}
-			else {
-				upd.setStatus(SUCCESS);
-			}
-			upd.setEnd(e);
-			hub().emitTrace(upd);
-			if(upd instanceof AbstractSessionUpdate ctx) {
-				clearContext(ctx);
-			}
-		};
+	@Override
+	public void handle(Instant start, Instant end, T obj, Throwable thrw) throws Exception {
+		listener.handle(start, end, obj, thrw);
 	}
 }
